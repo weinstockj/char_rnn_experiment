b95bef40eafae0be88136928f4966804190bb360
Josh Weinstock <josh82093@gmail.com>
2016-07-19 02:06:17 +0000

    adding scripts to download training data

--- sample.py
+++ sample.py
@@ -36,7 +36,7 @@ def sample(args):
         ckpt = tf.train.get_checkpoint_state(args.save_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
-            print(model.sample(sess, chars, vocab, args.n, args.prime, args.sample))
+            print(model.sample(sess, chars, vocab, args.n, args.prime, args.sample).encode('utf-8'))
 
 if __name__ == '__main__':
     main()
